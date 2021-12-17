1b3bdb0f7c3fcad94a2888d7269e9bc71c19f8c3
TomasProkes <tomas.prokes@centrum.cz>
2021-12-17 20:03:21 +0100

    fix theme replacement error in snow option

--- src/components/SelectSnowOption/index.jsx
+++ src/components/SelectSnowOption/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SelectSnowOption = ({handleSnow, desc, id, className, isChecked, snowVal}) => (
   <div className="field__radio">
-    <input onChange={(e) => handleSnow(e, snowVal)} type="radio" name="snow" id={theme} checked={isChecked} />
+    <input onChange={(e) => handleSnow(e, snowVal)} type="radio" name="snow" id={id} checked={isChecked} />
     <label for={id} className={className}>{desc}</label>
   </div>
 );
