2299f89888d832b2712a843f454f65942bbbedde
TomasProkes <tomas.prokes@centrum.cz>
2021-12-17 19:44:48 +0100

    fix cover image and snow intensity selectors

--- src/components/SelectCoverOption/index.jsx
+++ src/components/SelectCoverOption/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const SelectCoverOption = ({handleCover, desc, id, className, isChecked, coverVal}) => {
-  return (
-    <div className="field__swatch">
-      <input onChange={(e) => handleCover(e, coverVal)} type="radio" name="cover" id={id} checked={isChecked} />
-      <label for={id} className={className} data-description={desc}></label>
-    </div>
-  )
-};
+const SelectCoverOption = ({handleCover, desc, id, className, isChecked, coverVal}) => (
+  <div className="field__swatch">
+    <input onChange={(e) => handleCover(e, coverVal)} type="radio" name="cover" id={id} checked={isChecked} />
+    <label for={id} className={className} data-description={desc}></label>
+  </div>
+);
 
 export default SelectCoverOption;
--- src/components/SelectSnow/index.jsx
+++ src/components/SelectSnow/index.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import SelectSnowOption from '../SelectSnowOption';
 
-const SelectSnow = ({handleSnow, snowSet, snow}) => {
-  const desc_snow_0 = 'bez sněhu'
-  const desc_snow_1 = 'málo'
-  const desc_snow_2 = 'hodně'
-  
-  return (
-    <div className="field">
-      <label className="field__label ">Sníh na pozadí</label>
+const SelectSnow = ({handleSnow, snowSet, selection}) => (
+  <div className="field">
+    <label className="field__label ">Sníh na pozadí</label>
 
-      <SelectSnowOption intensity={0} desc='bez sněhu' is_checked={true} />
-      <SelectSnowOption intensity={100} desc='málo' />
-      <SelectSnowOption intensity={500} desc='hodně' />
+    {
+      snowSet.map(snow => {
+        const className = `radio--snow`
+        const id = `snow-${Math.ceil(snow.value / 250)}`
+        const isChecked = snow.value === selection
+        return <SelectSnowOption handleSnow={handleSnow} desc={snow.description} className={className} id={id} isChecked={isChecked} snowVal={snow.value} />
+      })
+    }
 
-    </div>
-  )
-};
+  </div>
+);
 
 export default SelectSnow;
