f702ec94197f99c06aa5ffe07d53b7c29c8b421c
TomasProkes <tomas.prokes@centrum.cz>
2021-12-19 00:02:29 +0100

    apply classname in music radio buttons

--- src/components/SelectMusic/index.jsx
+++ src/components/SelectMusic/index.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import SelectMusicOption from '../SelectMusicOption';
 
 const SelectMusic = ({handleMusic, musicSet, selection}) => {
-  const desc_jingle_bells = 'Rolničky, rolničky'
-  const desc_silent_night = 'Tichá noc'
-  const desc_god_rest = 'Pokoj Vám'
+  const className = `radio--music`
   
   return (
     <div className="field">
       <label className="field__label">Hudba</label>
-
       <div className="field__radio-group">
         {
           musicSet.map(music => {
-            const className = `radio--music`
             const id = `music-${music.value}`
             const isChecked = music.value === selection
             return <SelectMusicOption key={id} handleMusic={handleMusic} desc={music.description} className={className} id={id} isChecked={isChecked} musicVal={music.value} />
